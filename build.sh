930570c891775d4aa1196ae1845d93f0f440da1a
Alan Storm <astorm@alanstorm.com>
2016-03-20 16:59:43 -0700

    making sed replace composer.json

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sed 's/\("require": {\)/\1"pulsestorm\/pestle":"dev-master",/' composer.json
+sed 's/\("require": {\)/\1"pulsestorm\/pestle":"dev-master",/' composer.json > composer.json
 composer install
 curl -LO http://pestle.pulsestorm.net/pestle.phar
