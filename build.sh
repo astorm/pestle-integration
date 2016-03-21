4b89f543cde1a91bd282861b256bf820b2aedab7
Alan Storm <astorm@alanstorm.com>
2016-03-20 17:26:39 -0700

    making sure repo is added

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 sed 's/\("require": {\)/\1"pulsestorm\/pestle":"dev-master",/' magento2/composer.json > magento2/composer.json.notatomic
 mv magento2/composer.json.notatomic magento2/composer.json
+
+sed 's/\("name": "magento\/magento2ce",\)/"\1repositories": {"astorm\/pestle": {"type": "git","url": "git@github.com:astorm\/pestle.git"}},/' magento2/composer.json > magento2/composer.json.notatomic
+mv magento2/composer.json.notatomic magento2/composer.json
+
+
