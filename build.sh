ed0aa841047dda16ccc87a973a0c703e7f06e9fb
Alan Storm <astorm@alanstorm.com>
2016-03-20 17:30:01 -0700

    misplaced quote

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 sed 's/\("require": {\)/\1"pulsestorm\/pestle":"dev-master",/' magento2/composer.json > magento2/composer.json.notatomic
 mv magento2/composer.json.notatomic magento2/composer.json
 
-sed 's/\("name": "magento\/magento2ce",\)/"\1repositories": {"astorm\/pestle": {"type": "git","url": "git@github.com:astorm\/pestle.git"}},/' magento2/composer.json > magento2/composer.json.notatomic
+sed 's/\("name": "magento\/magento2ce",\)/\1"repositories": {"astorm\/pestle": {"type": "git","url": "git@github.com:astorm\/pestle.git"}},/' magento2/composer.json > magento2/composer.json.notatomic
 mv magento2/composer.json.notatomic magento2/composer.json
 
 
