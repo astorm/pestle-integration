51d689184f8a4b8ce5bc8bf7d4a8ae6ca72f9720
Alan Storm <astorm@alanstorm.com>
2016-03-21 15:32:16 -0700

    more more more

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sed 's/\("require": {\)/\1"pulsestorm\/pestle":"dev-master",/' magento2/composer.json > magento2/composer.json.notatomic
-mv magento2/composer.json.notatomic magento2/composer.json
-
 sed 's/\("name": "magento\/magento2ce",\)/\1"repositories": {"astorm\/pestle": {"type": "git","url": "git@github.com:astorm\/pestle.git"}},/' magento2/composer.json > magento2/composer.json.notatomic
 mv magento2/composer.json.notatomic magento2/composer.json
 
