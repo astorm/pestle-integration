6a291dfb7dc2e6f6dc2f3a7954d45bf3b72b2118
Alan Storm <astorm@alanstorm.com>
2016-03-21 18:24:41 -0700

    Trying composer manipulation

---
+++ addRepositoryToComposer.php
@@ -0,0 +1,42 @@
+<?php
+function createKeyFromUrl($url)
+{
+    return preg_replace('%[^a-zA-Z0-9]%', '_', $url);
+}
+
+function createRepositoryObjectFromInformation($type, $url)
+{
+    $object         = new stdClass;
+    $object->type   = $type;
+    $object->url    = $url;
+    return $object;
+}
+
+function main($argv)
+{
+    //     "repositories": {
+    //         "astorm/pestle": {
+    //             "type": "git",
+    //             "url": "git@github.com:astorm/pestle.git"
+    //         }
+    //     }, 
+    
+    $file            = 'magento2/composer.json';
+    $repository_type = 'git';
+    $repository_url  = 'git@github.com:astorm/pestle.git';    
+    $repository_key  = createKeyFromUrl($repository_url);
+
+    $repository = createRepositoryObjectFromInformation(
+        $repository_type, $repository_url);
+    
+    $json = json_decode(file_get_contents($file));
+    if(!isset($json->repositories))
+    {
+        $json->repositories = new stdClass;
+    }
+    
+    $json->repositories->{$repository_key} = $repository;
+
+    echo json_encode($json),"\n";
+}
+main($argv);
